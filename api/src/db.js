57eb37da8ff214b17ec38f0c89a66b713329561a
sdin2 <desat_94@hotmail.com>
2023-05-25 02:08:16 -0300

    Update db.js

--- api/src/db.js
+++ api/src/db.js
@@ -8,6 +8,12 @@ const sequelize = new Sequelize(
   DATABASE_URL,  {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+    ialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   }
 );
 const basename = path.basename(__filename);
